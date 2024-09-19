99d03e2418c82fa1399e635d977b32dbdf012a4c
mirzakhmets <mirzakhmets@icloud.com>
2024-09-19 13:02:13 +0500

    Added trial

--- MainForm.cs
+++ MainForm.cs
@@ -3,6 +3,7 @@ using System;
 using System.ComponentModel;
 using System.Drawing;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace MLPlus
 {
@@ -21,6 +22,47 @@ namespace MLPlus
     private TabPage tabPageMain;
     private TabControl tabControl;
 
+    public void CheckRuns() {
+		try {
+			RegistryKey key = Registry.CurrentUser.OpenSubKey("Software\\OVG-Developers", true);
+			
+			int runs = -1;
+			
+			if (key != null && key.GetValue("Runs") != null) {
+				runs = (int) key.GetValue("Runs");
+			} else {
+				key = Registry.CurrentUser.CreateSubKey("Software\\OVG-Developers");
+			}
+			
+			runs = runs + 1;
+			
+			key.SetValue("Runs", runs);
+			
+			if (runs > 10) {
+				System.Windows.Forms.MessageBox.Show("Number of runs expired.\n"
+							+ "Please register the application (visit https://ovg-developers.mystrikingly.com/ for purchase).");
+				
+				Environment.Exit(0);
+			}
+		} catch (Exception e) {
+			Console.WriteLine(e.Message);
+		}
+	}
+	
+	public bool IsRegistered() {
+		try {
+			RegistryKey key = Registry.CurrentUser.OpenSubKey("Software\\OVG-Developers");
+			
+			if (key != null && key.GetValue("Registered") != null) {
+				return true;
+			}
+		} catch (Exception e) {
+			Console.WriteLine(e.Message);
+		}
+		
+		return false;
+	}
+    
     public MainForm() {
     	this.InitializeComponent();
     }
@@ -74,109 +116,165 @@ namespace MLPlus
 
     private void InitializeComponent()
     {
-      ComponentResourceManager resources = new ComponentResourceManager(typeof (MainForm));
-      this.tabControl = new TabControl();
-      this.tabPageMain = new TabPage();
-      this.numericUpDownThreshold = new NumericUpDown();
-      this.labelThreshold = new Label();
-      this.buttonSearch = new Button();
-      this.richTextBoxData = new RichTextBox();
-      this.labelData = new Label();
-      this.textBoxQuery = new TextBox();
-      this.labelQuery = new Label();
-      this.tabPageResult = new TabPage();
-      this.richTextBoxResult = new RichTextBox();
-      this.tabControl.SuspendLayout();
-      this.tabPageMain.SuspendLayout();
-      this.numericUpDownThreshold.BeginInit();
-      this.tabPageResult.SuspendLayout();
-      this.SuspendLayout();
-      this.tabControl.Controls.Add((Control) this.tabPageMain);
-      this.tabControl.Controls.Add((Control) this.tabPageResult);
-      this.tabControl.Location = new Point(4, 4);
-      this.tabControl.Name = "tabControl";
-      this.tabControl.SelectedIndex = 0;
-      this.tabControl.Size = new Size(452, 323);
-      this.tabControl.TabIndex = 0;
-      this.tabPageMain.Controls.Add((Control) this.numericUpDownThreshold);
-      this.tabPageMain.Controls.Add((Control) this.labelThreshold);
-      this.tabPageMain.Controls.Add((Control) this.buttonSearch);
-      this.tabPageMain.Controls.Add((Control) this.richTextBoxData);
-      this.tabPageMain.Controls.Add((Control) this.labelData);
-      this.tabPageMain.Controls.Add((Control) this.textBoxQuery);
-      this.tabPageMain.Controls.Add((Control) this.labelQuery);
-      this.tabPageMain.Location = new Point(4, 22);
-      this.tabPageMain.Name = "tabPageMain";
-      this.tabPageMain.Padding = new Padding(3);
-      this.tabPageMain.Size = new Size(444, 297);
-      this.tabPageMain.TabIndex = 0;
-      this.tabPageMain.Text = "Main";
-      this.tabPageMain.UseVisualStyleBackColor = true;
-      this.numericUpDownThreshold.Location = new Point(83, 75);
-      this.numericUpDownThreshold.Name = "numericUpDownThreshold";
-      this.numericUpDownThreshold.Size = new Size(59, 20);
-      this.numericUpDownThreshold.TabIndex = 6;
-      this.labelThreshold.Location = new Point(19, 77);
-      this.labelThreshold.Name = "labelThreshold";
-      this.labelThreshold.Size = new Size(58, 22);
-      this.labelThreshold.TabIndex = 5;
-      this.labelThreshold.Text = "Threshold:";
-      this.buttonSearch.Location = new Point(188, 261);
-      this.buttonSearch.Name = "buttonSearch";
-      this.buttonSearch.Size = new Size(75, 23);
-      this.buttonSearch.TabIndex = 4;
-      this.buttonSearch.Text = "Search";
-      this.buttonSearch.UseVisualStyleBackColor = true;
-      this.buttonSearch.Click += new EventHandler(this.ButtonSearchClick);
-      this.richTextBoxData.Location = new Point(19, 137);
-      this.richTextBoxData.Name = "richTextBoxData";
-      this.richTextBoxData.Size = new Size(409, 118);
-      this.richTextBoxData.TabIndex = 3;
-      this.richTextBoxData.Text = "Potato,1.0,2.0\nOnion,3.0,4.0";
-      this.labelData.Location = new Point(19, 114);
-      this.labelData.Name = "labelData";
-      this.labelData.Size = new Size(51, 20);
-      this.labelData.TabIndex = 2;
-      this.labelData.Text = "Data:";
-      this.labelData.Click += new EventHandler(this.LabelDataClick);
-      this.textBoxQuery.Location = new Point(19, 40);
-      this.textBoxQuery.Name = "textBoxQuery";
-      this.textBoxQuery.Size = new Size(409, 20);
-      this.textBoxQuery.TabIndex = 1;
-      this.textBoxQuery.Text = "1.0,2.0";
-      this.labelQuery.Location = new Point(19, 18);
-      this.labelQuery.Name = "labelQuery";
-      this.labelQuery.Size = new Size(84, 19);
-      this.labelQuery.TabIndex = 0;
-      this.labelQuery.Text = "Query:";
-      this.tabPageResult.Controls.Add((Control) this.richTextBoxResult);
-      this.tabPageResult.Location = new Point(4, 22);
-      this.tabPageResult.Name = "tabPageResult";
-      this.tabPageResult.Padding = new Padding(3);
-      this.tabPageResult.Size = new Size(444, 297);
-      this.tabPageResult.TabIndex = 1;
-      this.tabPageResult.Text = "Result";
-      this.tabPageResult.UseVisualStyleBackColor = true;
-      this.richTextBoxResult.Location = new Point(6, 6);
-      this.richTextBoxResult.Name = "richTextBoxResult";
-      this.richTextBoxResult.Size = new Size(432, 285);
-      this.richTextBoxResult.TabIndex = 0;
-      this.richTextBoxResult.Text = "";
-      this.AutoScaleDimensions = new SizeF(6f, 13f);
-      this.AutoScaleMode = AutoScaleMode.Font;
-      this.ClientSize = new Size(461, 329);
-      this.Controls.Add((Control) this.tabControl);
-      this.Icon = (Icon) resources.GetObject("$this.Icon");
-      this.MaximizeBox = false;
-      this.MinimizeBox = false;
-      this.Name = "MainForm";
-      this.Text = "ML+";
-      this.tabControl.ResumeLayout(false);
-      this.tabPageMain.ResumeLayout(false);
-      this.tabPageMain.PerformLayout();
-      this.numericUpDownThreshold.EndInit();
-      this.tabPageResult.ResumeLayout(false);
-      this.ResumeLayout(false);
+    	System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+    	this.tabControl = new System.Windows.Forms.TabControl();
+    	this.tabPageMain = new System.Windows.Forms.TabPage();
+    	this.numericUpDownThreshold = new System.Windows.Forms.NumericUpDown();
+    	this.labelThreshold = new System.Windows.Forms.Label();
+    	this.buttonSearch = new System.Windows.Forms.Button();
+    	this.richTextBoxData = new System.Windows.Forms.RichTextBox();
+    	this.labelData = new System.Windows.Forms.Label();
+    	this.textBoxQuery = new System.Windows.Forms.TextBox();
+    	this.labelQuery = new System.Windows.Forms.Label();
+    	this.tabPageResult = new System.Windows.Forms.TabPage();
+    	this.richTextBoxResult = new System.Windows.Forms.RichTextBox();
+    	this.tabControl.SuspendLayout();
+    	this.tabPageMain.SuspendLayout();
+    	((System.ComponentModel.ISupportInitialize)(this.numericUpDownThreshold)).BeginInit();
+    	this.tabPageResult.SuspendLayout();
+    	this.SuspendLayout();
+    	// 
+    	// tabControl
+    	// 
+    	this.tabControl.Controls.Add(this.tabPageMain);
+    	this.tabControl.Controls.Add(this.tabPageResult);
+    	this.tabControl.Location = new System.Drawing.Point(5, 5);
+    	this.tabControl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+    	this.tabControl.Name = "tabControl";
+    	this.tabControl.SelectedIndex = 0;
+    	this.tabControl.Size = new System.Drawing.Size(603, 398);
+    	this.tabControl.TabIndex = 0;
+    	// 
+    	// tabPageMain
+    	// 
+    	this.tabPageMain.Controls.Add(this.numericUpDownThreshold);
+    	this.tabPageMain.Controls.Add(this.labelThreshold);
+    	this.tabPageMain.Controls.Add(this.buttonSearch);
+    	this.tabPageMain.Controls.Add(this.richTextBoxData);
+    	this.tabPageMain.Controls.Add(this.labelData);
+    	this.tabPageMain.Controls.Add(this.textBoxQuery);
+    	this.tabPageMain.Controls.Add(this.labelQuery);
+    	this.tabPageMain.Location = new System.Drawing.Point(4, 25);
+    	this.tabPageMain.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+    	this.tabPageMain.Name = "tabPageMain";
+    	this.tabPageMain.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+    	this.tabPageMain.Size = new System.Drawing.Size(595, 369);
+    	this.tabPageMain.TabIndex = 0;
+    	this.tabPageMain.Text = "Main";
+    	this.tabPageMain.UseVisualStyleBackColor = true;
+    	// 
+    	// numericUpDownThreshold
+    	// 
+    	this.numericUpDownThreshold.Location = new System.Drawing.Point(111, 92);
+    	this.numericUpDownThreshold.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+    	this.numericUpDownThreshold.Name = "numericUpDownThreshold";
+    	this.numericUpDownThreshold.Size = new System.Drawing.Size(79, 22);
+    	this.numericUpDownThreshold.TabIndex = 6;
+    	// 
+    	// labelThreshold
+    	// 
+    	this.labelThreshold.Location = new System.Drawing.Point(25, 95);
+    	this.labelThreshold.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+    	this.labelThreshold.Name = "labelThreshold";
+    	this.labelThreshold.Size = new System.Drawing.Size(77, 27);
+    	this.labelThreshold.TabIndex = 5;
+    	this.labelThreshold.Text = "Threshold:";
+    	// 
+    	// buttonSearch
+    	// 
+    	this.buttonSearch.Location = new System.Drawing.Point(251, 321);
+    	this.buttonSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+    	this.buttonSearch.Name = "buttonSearch";
+    	this.buttonSearch.Size = new System.Drawing.Size(100, 28);
+    	this.buttonSearch.TabIndex = 4;
+    	this.buttonSearch.Text = "Search";
+    	this.buttonSearch.UseVisualStyleBackColor = true;
+    	this.buttonSearch.Click += new System.EventHandler(this.ButtonSearchClick);
+    	// 
+    	// richTextBoxData
+    	// 
+    	this.richTextBoxData.Location = new System.Drawing.Point(25, 169);
+    	this.richTextBoxData.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+    	this.richTextBoxData.Name = "richTextBoxData";
+    	this.richTextBoxData.Size = new System.Drawing.Size(544, 144);
+    	this.richTextBoxData.TabIndex = 3;
+    	this.richTextBoxData.Text = "Potato,1.0,2.0\nOnion,3.0,4.0";
+    	// 
+    	// labelData
+    	// 
+    	this.labelData.Location = new System.Drawing.Point(25, 140);
+    	this.labelData.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+    	this.labelData.Name = "labelData";
+    	this.labelData.Size = new System.Drawing.Size(68, 25);
+    	this.labelData.TabIndex = 2;
+    	this.labelData.Text = "Data:";
+    	this.labelData.Click += new System.EventHandler(this.LabelDataClick);
+    	// 
+    	// textBoxQuery
+    	// 
+    	this.textBoxQuery.Location = new System.Drawing.Point(25, 49);
+    	this.textBoxQuery.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+    	this.textBoxQuery.Name = "textBoxQuery";
+    	this.textBoxQuery.Size = new System.Drawing.Size(544, 22);
+    	this.textBoxQuery.TabIndex = 1;
+    	this.textBoxQuery.Text = "1.0,2.0";
+    	// 
+    	// labelQuery
+    	// 
+    	this.labelQuery.Location = new System.Drawing.Point(25, 22);
+    	this.labelQuery.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+    	this.labelQuery.Name = "labelQuery";
+    	this.labelQuery.Size = new System.Drawing.Size(112, 23);
+    	this.labelQuery.TabIndex = 0;
+    	this.labelQuery.Text = "Query:";
+    	// 
+    	// tabPageResult
+    	// 
+    	this.tabPageResult.Controls.Add(this.richTextBoxResult);
+    	this.tabPageResult.Location = new System.Drawing.Point(4, 25);
+    	this.tabPageResult.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+    	this.tabPageResult.Name = "tabPageResult";
+    	this.tabPageResult.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+    	this.tabPageResult.Size = new System.Drawing.Size(595, 369);
+    	this.tabPageResult.TabIndex = 1;
+    	this.tabPageResult.Text = "Result";
+    	this.tabPageResult.UseVisualStyleBackColor = true;
+    	// 
+    	// richTextBoxResult
+    	// 
+    	this.richTextBoxResult.Location = new System.Drawing.Point(8, 7);
+    	this.richTextBoxResult.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+    	this.richTextBoxResult.Name = "richTextBoxResult";
+    	this.richTextBoxResult.Size = new System.Drawing.Size(575, 350);
+    	this.richTextBoxResult.TabIndex = 0;
+    	this.richTextBoxResult.Text = "";
+    	// 
+    	// MainForm
+    	// 
+    	this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+    	this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+    	this.ClientSize = new System.Drawing.Size(615, 405);
+    	this.Controls.Add(this.tabControl);
+    	this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+    	this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+    	this.MaximizeBox = false;
+    	this.MinimizeBox = false;
+    	this.Name = "MainForm";
+    	this.Text = "ML+";
+    	this.Shown += new System.EventHandler(this.MainFormShown);
+    	this.tabControl.ResumeLayout(false);
+    	this.tabPageMain.ResumeLayout(false);
+    	this.tabPageMain.PerformLayout();
+    	((System.ComponentModel.ISupportInitialize)(this.numericUpDownThreshold)).EndInit();
+    	this.tabPageResult.ResumeLayout(false);
+    	this.ResumeLayout(false);
+
     }
+		void MainFormShown(object sender, EventArgs e)
+		{
+			if (!IsRegistered()) {
+				CheckRuns();
+    		}
+		}
   }
 }
